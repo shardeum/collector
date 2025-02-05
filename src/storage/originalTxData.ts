51dc50a276e8c3fee6a5cafb6fc017794d9b1bbc
Aniket Divekar <aniket.divekar@gmail.com>
2025-02-05 22:26:13 +0530

    use txId & timestamp to create key for originalTxsMap. this will allow all data to be inserted in collector.

--- src/storage/originalTxData.ts
+++ src/storage/originalTxData.ts
@@ -74,11 +74,15 @@ export async function processOriginalTxData(
 
   for (const originalTxData of originalTxsData) {
     const { txId, timestamp } = originalTxData
-    if (originalTxsMap.has(txId) && originalTxsMap.get(txId) === timestamp) continue
-    originalTxsMap.set(txId, timestamp)
+    const key = `${txId}-${timestamp}`
+    if (originalTxsMap.has(key) && originalTxsMap.get(key) === timestamp) {
+      continue
+    }
+
+    originalTxsMap.set(key, timestamp)
     /* prettier-ignore */ if (config.verbose) console.log('originalTxData', originalTxData)
     if (saveOnlyNewData) {
-      const originalTxDataExist = await queryOriginalTxDataByTxId(txId)
+      const originalTxDataExist = await queryOriginalTxDataByTxIdAndTimestamp(txId, timestamp)
       if (originalTxDataExist) continue
     }
     combineOriginalTxsData.push(originalTxData)
@@ -176,12 +180,12 @@ export async function queryOriginalTxDataCount(
           txType === TransactionSearchType.Receipt
             ? TransactionType.Receipt
             : txType === TransactionSearchType.NodeRewardReceipt
-              ? TransactionType.NodeRewardReceipt
-              : txType === TransactionSearchType.StakeReceipt
-                ? TransactionType.StakeReceipt
-                : txType === TransactionSearchType.UnstakeReceipt
-                  ? TransactionType.UnstakeReceipt
-                  : TransactionType.InternalTxReceipt
+            ? TransactionType.NodeRewardReceipt
+            : txType === TransactionSearchType.StakeReceipt
+            ? TransactionType.StakeReceipt
+            : txType === TransactionSearchType.UnstakeReceipt
+            ? TransactionType.UnstakeReceipt
+            : TransactionType.InternalTxReceipt
         sql += ` transactionType=?`
         values.push(ty)
       }
@@ -234,12 +238,12 @@ export async function queryOriginalTxsData(
           txType === TransactionSearchType.Receipt
             ? TransactionType.Receipt
             : txType === TransactionSearchType.NodeRewardReceipt
-              ? TransactionType.NodeRewardReceipt
-              : txType === TransactionSearchType.StakeReceipt
-                ? TransactionType.StakeReceipt
-                : txType === TransactionSearchType.UnstakeReceipt
-                  ? TransactionType.UnstakeReceipt
-                  : TransactionType.InternalTxReceipt
+            ? TransactionType.NodeRewardReceipt
+            : txType === TransactionSearchType.StakeReceipt
+            ? TransactionType.StakeReceipt
+            : txType === TransactionSearchType.UnstakeReceipt
+            ? TransactionType.UnstakeReceipt
+            : TransactionType.InternalTxReceipt
         sql += ` transactionType=?`
         values.push(ty)
       }
@@ -268,6 +272,26 @@ export async function queryOriginalTxDataByTxId(txId: string): Promise<OriginalT
   try {
     const sql = `SELECT * FROM originalTxsData WHERE txId=?`
     const originalTxData: DbOriginalTxData = await db.get(sql, [txId])
+    if (originalTxData) {
+      if (originalTxData.originalTxData)
+        originalTxData.originalTxData = StringUtils.safeJsonParse(originalTxData.originalTxData)
+      if (originalTxData.sign) originalTxData.sign = StringUtils.safeJsonParse(originalTxData.sign)
+    }
+    if (config.verbose) console.log('OriginalTxData txId', originalTxData)
+    return originalTxData as unknown as OriginalTxDataInterface
+  } catch (e) {
+    console.log(e)
+  }
+  return null
+}
+
+export async function queryOriginalTxDataByTxIdAndTimestamp(
+  txId: string,
+  timestamp: Number
+): Promise<OriginalTxDataInterface | null> {
+  try {
+    const sql = `SELECT * FROM originalTxsData WHERE txId=? and timestamp=?`
+    const originalTxData: DbOriginalTxData = await db.get(sql, [txId, timestamp])
     if (originalTxData) {
       if (originalTxData.originalTxData)
         originalTxData.originalTxData = StringUtils.safeJsonParse(originalTxData.originalTxData)
