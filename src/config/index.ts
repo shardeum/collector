2b9b5fed5a7777e12342d1e2998cc58163d74ae7
Sam Sweet <samwsweet@gmail.com>
2025-06-18 10:32:36 -0400

    simplify saveReceiptsWithSignaturePacks config since we will set through config overwrite

--- src/config/index.ts
+++ src/config/index.ts
@@ -139,10 +139,7 @@ let config: Config = {
   saveAccountHistoryState: true,
   collectorMode: process.env.COLLECTOR_MODE || collectorMode.WS.toString(),
   storeReceiptBeforeStates: true,
-  saveReceiptsWithSignaturePacks:
-    process.env.SAVE_RECEIPTS_WITH_SIGNATURE_PACKS !== undefined
-      ? process.env.SAVE_RECEIPTS_WITH_SIGNATURE_PACKS === 'true'
-      : true,
+  saveReceiptsWithSignaturePacks: true,
   checkpointWindow: 21,
   chainId: process.env.CHAIN_ID || '8082',
 }
